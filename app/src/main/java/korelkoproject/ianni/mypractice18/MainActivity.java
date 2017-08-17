5c1284c4038f3a2be8fff294622b413ece15b7b8
ianni <iannistsoumas@gmail.com>
2017-08-17 10:05:33 +0300

    practice 18

--- app/src/main/java/korelkoproject/ianni/mypractice18/MainActivity.java
+++ app/src/main/java/korelkoproject/ianni/mypractice18/MainActivity.java
@@ -1,13 +1,143 @@
 package korelkoproject.ianni.mypractice18;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.pm.PackageManager;
+import android.media.MediaPlayer;
+import android.media.MediaRecorder;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import java.io.IOException;
+import java.util.Random;
+
+import static android.Manifest.permission.RECORD_AUDIO;
+import static android.Manifest.permission.WRITE_EXTERNAL_STORAGE;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button recordButton;
+    String AudioSavePathInDevice = null;
+    MediaRecorder mediaRecorder ;
+    Random random ;
+    String RandomAudioFileName = "ABCDEFGHIJKLMNOP";
+    public static final int RequestPermissionCode = 1;
+    MediaPlayer mediaPlayer ;
+    boolean isClicked = true;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        recordButton = (Button) findViewById(R.id.record_button);
+
+    }
+
+
+    public void onClick(View view) throws IOException {
+
+        if (isClicked)
+        {
+            if (checkPermission()) {
+
+                MediaRecorderReady();
+                startRecording();
+            }
+            else {
+                requestPermission();
+            }
+
+            isClicked = false;
+        }
+
+        else
+        {
+            stopRecording();
+            isClicked = true;
+        }
+    }
+
+
+
+    private void startRecording()
+    {
+        try {
+            mediaRecorder.prepare();
+            mediaRecorder.start();
+
+
+        } catch (IllegalStateException | IOException e) {
+            e.printStackTrace();
+        }
+
+        Toast.makeText(this,"Recording start",Toast.LENGTH_SHORT).show();
+    }
+
+
+
+    private void stopRecording()
+    {
+        try {
+            mediaRecorder.stop();
+        }
+        catch(RuntimeException ignored){}
+
+        Toast.makeText(MainActivity.this, "Recording stop",Toast.LENGTH_LONG).show();
+
+    }
+
+
+
+    public void MediaRecorderReady(){
+
+        mediaRecorder =new MediaRecorder();
+        mediaRecorder.setAudioSource(MediaRecorder.AudioSource.MIC);
+        mediaRecorder.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP);
+        mediaRecorder.setAudioEncoder(MediaRecorder.OutputFormat.AMR_NB);
+        mediaRecorder.setOutputFile(AudioSavePathInDevice);
+    }
+
+
+
+    private void requestPermission() {
+        ActivityCompat.requestPermissions(MainActivity.this, new
+                String[]{WRITE_EXTERNAL_STORAGE, RECORD_AUDIO}, RequestPermissionCode);
+    }
+
+
+    public boolean checkPermission() {
+
+        int result = ContextCompat.checkSelfPermission(getApplicationContext(),
+                WRITE_EXTERNAL_STORAGE);
+        int result1 = ContextCompat.checkSelfPermission(getApplicationContext(),
+                RECORD_AUDIO);
+        return result == PackageManager.PERMISSION_GRANTED &&
+                result1 == PackageManager.PERMISSION_GRANTED;
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case RequestPermissionCode:
+                if (grantResults.length> 0) {
+                    boolean StoragePermission = grantResults[0] ==
+                            PackageManager.PERMISSION_GRANTED;
+                    boolean RecordPermission = grantResults[1] ==
+                            PackageManager.PERMISSION_GRANTED;
+
+                    if (StoragePermission && RecordPermission) {
+                        Toast.makeText(MainActivity.this, "Permission Granted",
+                                Toast.LENGTH_LONG).show();
+                    } else {
+                        Toast.makeText(MainActivity.this,"Permission Denied",Toast.LENGTH_LONG).show();
+                    }
+                }
+                break;
+        }
     }
 }
+
